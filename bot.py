2490ea278387b38072fc33ba7088df9394f459e0
Just-Ty <foolgrimmer@gmail.com>
2026-06-24 10:13:46 +0100

    Initil setup

--- bot.py
+++ bot.py
@@ -1,1 +1,23 @@
+import discord
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == '!ping':
+        await message.channel.send('Pong!')
+
+client.run(os.getenv('DISCORD_TOKEN'))
